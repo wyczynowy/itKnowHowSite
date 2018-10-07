d2da0b4dfd5e8f47fa712f6ec1d3ab92aec4bcc3
wyczynowy <dawidkozub@gmail.com>
2018-10-07 22:09:56 +0200

    Oddanie źródeł na gitHub

---
+++ WebContent/scripts/wspolneSkrypty.js
@@ -0,0 +1,265 @@
+function loadHtmlIntoIframe(id, location) {
+		$('#'+id).attr('src',location);
+	}
+	
+	function checkSiteWidth () {
+		if(window.innerWidth < 700) {
+			$('.content-left').hide();
+			$('.content-right').hide();
+			$('.content-center').width('100%');
+		} else {
+			$('.content-left').show();
+			$('.content-right').show();
+			$('.content-center').width(document.body.offsetWidth - 300);
+		}
+	}
+	
+	var tablicaKolorowAnimacji = ['#990000','#9a0000','#aa0000','#ab0000','#bb0000','#bc0000'];
+	
+	function doSetActive(id,i){
+		setTimeout(function(){
+			id.style.fontSize = i+17+'px';
+			id.style.backgroundColor = tablicaKolorowAnimacji[i];
+		}, i*25);
+	}
+	
+	function setActive(id) {
+		for(var i = 0; i <= 5; i++){ doSetActive(id,i); }
+	}
+	
+	function doSetDisactive(id,i){
+		setTimeout(function(){
+			id.style.fontSize = 22-i+'px';
+			id.style.backgroundColor = tablicaKolorowAnimacji[5-i];
+		}, i*25);
+	}
+	
+	function setDisactive(id) {
+		for(var i = 0; i <= 5; i++ ){ doSetDisactive(id,i); }
+	}
+	
+	function setActiveSubButton(id){
+		var otherIdsTab = document.getElementsByClassName('subMenuButton');
+		for(var i = 0; i < otherIdsTab.length; i++){
+			otherIdsTab[i].style.backgroundColor = "#990000";
+		}
+		id.style.backgroundColor = "#bc0000";
+	}
+	
+  	function runFunctionAfterPageLoad(functionReference) {
+  		if (window.addEventListener)
+  			window.addEventListener("load", functionReference, false);
+  		else if (window.attachEvent)
+  			window.attachEvent("onload", functionReference);
+  		else window.onload = functionReference;
+  	}
+  	
+  	function setActiveMenuButton(numerPrzycisku){
+  			var przyciskiMenu = document.getElementsByClassName('menuButton');
+  	  		przyciskiMenu[numerPrzycisku].onmouseover = '';
+  	  		przyciskiMenu[numerPrzycisku].onmouseout = '';
+			przyciskiMenu[numerPrzycisku].style.fontSize = '22px';
+			przyciskiMenu[numerPrzycisku].style.backgroundColor = tablicaKolorowAnimacji[5];
+  	}
+  	
+  	var nazwaPrzycisku;
+  	function zaladujMenuIPodswietlPrzycisk(menuId, nazwaPrzycisku) {
+  		this.nazwaPrzycisku = nazwaPrzycisku;
+  		$(document).ready(function(){$('#' + menuId).load("../menu/menu.html");});
+  	}
+  	  	
+    function getBrowserHeight() {
+        var intH = 0;
+        var intW = 0;
+       
+
+        if(typeof window.innerWidth  == 'number' ) {
+           intH = window.innerHeight;
+           intW = window.innerWidth;
+        }
+        else if(document.documentElement && (document.documentElement.clientWidth || document.documentElement.clientHeight)) {
+            intH = document.documentElement.clientHeight;
+            intW = document.documentElement.clientWidth;
+        }
+        else if(document.body && (document.body.clientWidth || document.body.clientHeight)) {
+            intH = document.body.clientHeight;
+            intW = document.body.clientWidth;
+        }
+
+
+        return { width: parseInt(intW), height: parseInt(intH) };
+    }
+    
+    function dopasujObrazek(img) {
+    	var bws = getBrowserHeight();
+        img.style.position = "fixed";
+        if(bws.width <= img.naturalWidth) {
+        	img.style.width = "100%";
+        	img.style.height = "auto";
+        	img.style.left = "0px";
+        	img.style.top = parseInt((bws.height - img.height) / 2) + "px";
+        	if(bws.height <= img.height) {
+            	var wspProporcji = parseFloat(bws.height / img.naturalHeight);
+                img.style.left = parseFloat((bws.width - (img.naturalWidth * wspProporcji)) / 2) + "px";
+                img.style.top = "0px";
+            	img.style.height = bws.height + "px";
+            	img.style.width = parseInt(img.naturalWidth * wspProporcji) + "px";
+        	}
+        } else if(bws.height <= img.naturalHeight){
+        	var wspProporcji = parseFloat(bws.height / img.naturalHeight);
+            img.style.left = parseFloat((bws.width - (img.naturalWidth * wspProporcji)) / 2) + "px";
+            img.style.top = "0px";
+        	img.style.height = bws.height + "px";
+        	img.style.width = parseInt(img.naturalWidth * wspProporcji) + "px";
+        } else {
+            img.style.left = parseInt((bws.width - img.naturalWidth) / 2) + "px";
+            img.style.top = parseInt((bws.height - img.naturalHeight) / 2) + "px";
+        }    	
+    }
+    
+    function setLayerPosition() {
+
+        
+        if(imgZoomImage != null) {
+        	dopasujObrazek(imgZoomImage);
+            var shadow = document.getElementById("shadow");
+            var bws = getBrowserHeight();
+            shadow.style.width = bws.width + "px";
+            shadow.style.height = bws.height + "px";
+        }
+
+        shadow = null;
+    }
+    
+    var przelacznikZoomImage = false;
+    var imgZoomImage = null;
+    
+    function powiekszZdjecie(img) {
+    	var pojemnikNaZdjecie = document.getElementById('imgZoomDiv');
+    	var shadow = document.getElementById("shadow");
+    	var bws = getBrowserHeight();
+    	
+    	if(!przelacznikZoomImage) {
+        	pojemnikNaZdjecie.appendChild(img.cloneNode(true));
+        	dopasujObrazek(pojemnikNaZdjecie.childNodes[0]);
+    		imgZoomImage = pojemnikNaZdjecie.childNodes[0];
+    		
+            shadow.style.width = bws.width + "px";
+            shadow.style.height = bws.height + "px";
+            
+            shadow.style.display = "block";
+            
+            dopasujObrazek(pojemnikNaZdjecie.childNodes[0]);
+            przelacznikZoomImage = true;
+
+    	} else {
+    		shadow.style.display = "none";
+    		imgZoomImage.remove();
+    		przelacznikZoomImage = false;
+    		imgZoomImage = null;
+    	}
+    	
+    }
+    
+    function showLayer() {
+        setLayerPosition();
+
+
+        var shadow = document.getElementById("shadow");
+        var question = document.getElementById("question");
+
+
+        shadow.style.display = "block";
+        question.style.display = "block";
+
+
+        shadow = null;
+        question = null;
+    }
+    
+    function hideLayer() {
+        var shadow = document.getElementById("shadow");
+        var question = document.getElementById("question");
+
+
+        shadow.style.display = "none";
+        question.style.display = "none";
+
+
+        shadow = null;
+        question = null;
+    }
+    
+    function onResizeWindow(){
+    	setLayerPosition(); // Skalowanie przybliżonych obrazków
+    	skalujMenuGlowne(); // Skalowanie menu głównego
+    }
+    
+    var ukrytePrzyciskiMenuArray = new Array();
+    
+    function skalujMenuGlowne() {
+    	var menuDivInsideMenuDiv = document.getElementById('menuDivInsideMenuDiv');
+    	var menuButtonDiv = document.getElementsByClassName('menuButtonDiv');
+    	var menuButton = document.getElementsByClassName('menuButton');
+    	
+    	var iloscMozliwychDoWyswietleniaPrzyciskowMenu = parseInt((window.innerWidth - 50)/102);
+    	var iloscAktualnieWyswietlanychPrzyciskowMenu = 0; // parseInt(menuDivInsideMenuDiv.style.width.substring(0,menuDivInsideMenuDiv.style.width.length - 2)/102);
+    	
+    	ukrytePrzyciskiMenuArray.forEach(function(element){if(element == true){iloscAktualnieWyswietlanychPrzyciskowMenu++}});
+    	
+    	menuDivInsideMenuDiv.style.width = iloscMozliwychDoWyswietleniaPrzyciskowMenu > menuButtonDiv.length ? (menuButtonDiv.length * 102) + 'px' : (iloscMozliwychDoWyswietleniaPrzyciskowMenu * 102) + 'px';
+    	menuDivInsideMenuDiv.style.margin = '0 auto;'
+
+    	// Wyświetlamy potrzebne przyciski
+    	for(var i = 0; i < (iloscMozliwychDoWyswietleniaPrzyciskowMenu > menuButtonDiv.length ? menuButtonDiv.length : iloscMozliwychDoWyswietleniaPrzyciskowMenu - 1) ; i++) {
+    		menuButtonDiv[i].style.display = 'inline-block';
+    		ukrytePrzyciskiMenuArray[i] = true;
+    	}
+    	
+    	// Chowamy zbędne przyciski
+    	for(var i = iloscMozliwychDoWyswietleniaPrzyciskowMenu - 1; i < menuButtonDiv.length; i++) {
+    		menuButtonDiv[i].style.display = 'none';
+    		ukrytePrzyciskiMenuArray[i] = false;
+    		// document.getElementById('extendedMenuDiv').appendChild(menuButtonDiv[i].cloneNode(true));
+    	}
+    	
+    	var przyciskDodatkowyMenu = document.getElementsByClassName('extendedMenuButtonDiv')[0];
+    	if(iloscMozliwychDoWyswietleniaPrzyciskowMenu - 1 < menuButtonDiv.length) {
+    		przyciskDodatkowyMenu.style.display = 'inline-block';
+    	} else {
+    		przyciskDodatkowyMenu.style.display = 'none';
+    	}
+    	
+    	// Usuniecie poprzednio skopiowanych elementow do kontenera o id = 'extendedMenuDiv'
+    	var lvExtendedMenuDiv = document.getElementById('extendedMenuDiv');
+    	var lvIloscElementowDoUsuniecia = lvExtendedMenuDiv.childNodes.length;
+    	for(var i = 2; i < lvIloscElementowDoUsuniecia; i++) {
+	    		lvExtendedMenuDiv.childNodes[lvExtendedMenuDiv.childNodes.length - 1].remove(true);
+    	}
+    	ukrytePrzyciskiMenuArray.forEach(function(element,pos){
+    		if(!element){
+    			document.getElementById('extendedMenuDiv').appendChild(menuButtonDiv[pos].cloneNode(true));
+    		}
+    		});
+    	
+    }
+    
+    function pokazNiewidocznePrzyciski() {
+    	var kontener = document.getElementById('extendedMenuDiv');
+    	var wysokosc = (kontener.childNodes.length - 1) * 40;
+    	kontener.style.height = wysokosc + 'px';
+    	for(var i = 2; i < kontener.childNodes.length; i++) {
+    		kontener.childNodes[i].style.display = 'inline-block';
+    		kontener.childNodes[i].style.width = '98px';
+    	}
+    	kontener.onmouseleave = ukryjNiewidocznePrzyciski;
+    	kontener.zIndex = 1001;
+    }
+    
+	function ukryjNiewidocznePrzyciski() {
+		var kontener = document.getElementById('extendedMenuDiv');
+		kontener.style.height = '40px';
+    	for(var i = 2; i < kontener.childNodes.length; i++) {
+    		kontener.childNodes[i].style.display = 'none';
+    	}
+	}
